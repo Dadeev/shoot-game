edb94f261c589edcec2ffcd398cf52e5d4ee59f8
dadeev <dadeev.tim@gmail.com>
2023-01-14 19:12:34 +0700

    feat: implement a flight of projectiles

--- index.js
+++ index.js
@@ -35,15 +35,43 @@ class Projectile {
         ctx.fillStyle = this.color
         ctx.fill()
     }
+
+    update() {
+        this.draw()
+        this.x += this.velocity.x
+        this.y += this.velocity.y
+    }
 }
 
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, '#4767d1')
-player.draw()
+
+const projectiles = []
+
+function animate() {
+    requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+
+    projectiles.forEach((projectile) => {
+        projectile.update()
+    })
+}
+
+animate()
 
 addEventListener('click', (e) => {
-    const projectile = new Projectile(e.clientX, e.clientY, 5, 'red', null)
-    projectile.draw()
+    const angle = Math.atan2(e.clientY - canvas.height / 2, e.clientX - canvas.width / 2)
+
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+
+    projectiles.push(
+        new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
+    )
 })
+
