85fda25a6bed9ffef64b3a28cdb9f9d87e4ee178
dadeev <dadeev.tim@gmail.com>
2023-01-14 20:07:37 +0700

    feat: create enemies

--- index.js
+++ index.js
@@ -43,12 +43,36 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x += this.velocity.x
+        this.y += this.velocity.y
+    }
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, '#4767d1')
 
 const projectiles = []
+const enemies = []
 
 function animate() {
     requestAnimationFrame(animate)
@@ -58,13 +82,38 @@ function animate() {
     projectiles.forEach((projectile) => {
         projectile.update()
     })
+
+    enemies.forEach((enemy) => {
+        enemy.update()
+    })
+}
+
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 4) + 4
+        let x;
+        let y;
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+        } else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+        const color = 'green'
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+    }, 1000)
 }
 
 animate()
 
 addEventListener('click', (e) => {
     const angle = Math.atan2(e.clientY - canvas.height / 2, e.clientX - canvas.width / 2)
-
     const velocity = {
         x: Math.cos(angle),
         y: Math.sin(angle)
@@ -75,3 +124,4 @@ addEventListener('click', (e) => {
     )
 })
 
+spawnEnemies()
