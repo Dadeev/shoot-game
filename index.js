dc827d616c03fab8c838235068e5014c223a3758
dadeev <dadeev.tim@gmail.com>
2023-01-14 18:11:49 +0700

    feat: implement projectiles

--- index.js
+++ index.js
@@ -20,8 +20,30 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, '#4767d1')
 player.draw()
+
+addEventListener('click', (e) => {
+    const projectile = new Projectile(e.clientX, e.clientY, 5, 'red', null)
+    projectile.draw()
+})
