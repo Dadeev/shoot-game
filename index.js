6b7e7a8b3df3c7e34818ea3da363661c0bf9c583
dadeev <dadeev.tim@gmail.com>
2023-01-15 15:43:24 +0700

    feat: implement particle explosion on hit

--- index.js
+++ index.js
@@ -66,6 +66,38 @@ class Enemy {
     }
 }
 
+const friction = 0.99
+
+class Particle {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+        this.alpha = 1
+    }
+
+    draw() {
+        ctx.save()
+        ctx.globalAlpha = this.alpha
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+        ctx.fillStyle = this.color
+        ctx.fill()
+        ctx.restore()
+    }
+
+    update() {
+        this.draw()
+        this.velocity.x *= friction
+        this.velocity.y *= friction
+        this.x += this.velocity.x
+        this.y += this.velocity.y
+        this.alpha -= 0.01
+    }
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
@@ -73,7 +105,7 @@ const player = new Player(x, y, 10, '#e5e2e2')
 
 const projectiles = []
 const enemies = []
-
+const particles = []
 
 let animationId;
 
@@ -82,6 +114,14 @@ function animate() {
     ctx.fillStyle = 'rgba(0,0,0,0.1)'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
+
+    particles.forEach((particle, partIndex) => {
+        if (particle.alpha <= 0) {
+            particles.splice(partIndex, 1)
+        } else {
+            particle.update()
+        }
+    })
 
     projectiles.forEach((projectile, projIndex) => {
         projectile.update()
@@ -108,6 +148,15 @@ function animate() {
         projectiles.forEach((projectile, projIndex) => {
             const projectileAndEnemyDist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
             if (projectileAndEnemyDist - projectile.radius - enemy.radius < 1) {
+                for (let i = 0; i < enemy.radius; i++) {
+                    particles.push(
+                        new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color, {
+                            x: (Math.random() - 0.5) * (Math.random() * 6),
+                            y: (Math.random() - 0.5) * (Math.random() * 6)
+                        })
+                    )
+                }
+
                 if (enemy.radius - 10 > 5) {
                     gsap.to(enemy, {
                         radius: enemy.radius - 10
